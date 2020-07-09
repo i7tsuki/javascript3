3c748127e007091af81011b3e3e2ca8e1f2d413c
i7tsuki <tanaka_yi7@yahoo.co.jp>
2020-07-09 20:14:41 +0900

    javascript3-3

---
+++ script.js
@@ -0,0 +1,84 @@
+let id = 1;
+
+function addTask() {
+ 
+ 	let htmlOutputArray = [];
+ 	
+ 	
+    //ID（1列目）
+    let td1 = document.createElement("td");
+    let td1TextNode = document.createTextNode(id);
+    td1.appendChild(td1TextNode);
+    htmlOutputArray.push(td1);
+    
+    
+    //入力データ（2列目）
+    const taskValue = document.getElementById('taskValue').value;
+    let td2 = document.createElement("td");
+    let td2TextNode = document.createTextNode(taskValue);
+    td2.appendChild(td2TextNode);
+    htmlOutputArray.push(td2);
+    
+    
+    //未完了等のステータス（3列目）
+    let statusButton = document.createElement("input");
+    statusButton.setAttribute("type", "button");
+    statusButton.setAttribute("value", "作業中");
+    statusButton.setAttribute("id", "btn" + id);
+    statusButton.setAttribute("onclick", 'changeStatus("btn' + id +'");');
+    htmlOutputArray.push(statusButton);
+    
+    //削除ボタン（4列目）
+    let delButton = document.createElement("input");
+    delButton.setAttribute("type", "button");
+    delButton.setAttribute("value", "削除");
+    delButton.setAttribute("onclick", "delTask(" + id + ");");
+    htmlOutputArray.push(delButton);
+    
+    //出力
+	let tr = document.createElement("tr");
+	tr.setAttribute("id", id);
+	
+	htmlOutputArray.forEach((e) => {
+	    tr.appendChild(e);
+	});
+    
+    let table = document.getElementById("table");	
+    table.appendChild(tr);
+    
+    //事後処理
+    document.getElementById("taskValue").value = "";
+    id += 1;
+}
+
+function delTask(delId) {
+	//削除
+	let table = document.getElementById("table");
+	let tr = document.getElementById(delId);
+	table.removeChild(tr);
+	
+	
+	//採番
+	id = 1;
+	
+	for (let i=2; i<table.childNodes.length; i++) {	//id更新、i=2以降が対象
+		table.childNodes[i].setAttribute("id", id); //id属性
+		table.childNodes[i].firstChild.textContent = id; 	//ID（1列目）
+		table.childNodes[i].childNodes[2].setAttribute("id", "btn" + id); //ステータス（3列目）①
+		table.childNodes[i].childNodes[2].setAttribute("onclick", 'changeStatus("btn' + id +'");'); //ステータス（3列目）②
+		table.childNodes[i].lastChild.setAttribute("onclick", "delTask(" + id + ")");	//削除ボタン（4列目）
+		id += 1;
+	}
+}
+
+function changeStatus(changeId) {
+	let btn = document.getElementById(changeId);
+	
+	console.log(typeof changeId);
+	
+	if (btn.getAttribute("value") === "作業中") {
+		btn.setAttribute("value", "完了");
+	} else {
+		btn.setAttribute("value", "作業中");
+	}
+} 
